9af28e158c1c262dcd8bb116f3e9224f56cba6e6
DanilKozlov1 <kdanyl3103@gmail.com>
2026-02-09 11:27:24 +0700

    Обновление API ведра с болтами

--- API-BucketWithBolts/API-BucketWithBolts/Pages/Index.cshtml.cs
+++ API-BucketWithBolts/API-BucketWithBolts/Pages/Index.cshtml.cs
@@ -6,10 +6,12 @@ namespace API_BucketWithBolts.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Database _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, Database context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
