619ca5a95ddf177d308222d60e41d3b922432719
KarasiKoff <142608861+KarasiKoff@users.noreply.github.com>
2026-02-09 06:57:15 +0300

    react base

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/header/Header';
+import Main from './components/main/Main';
+
 export default function App() {
   return (
     <>
       <Header />
       <h1>Hello world!</h1>
+      <Main />
     </>
   );
 }
--- react-app/src/components/main/Main.jsx
+++ react-app/src/components/main/Main.jsx
@@ -1,20 +1,23 @@
 import './Main.css';
 import axios from "axios"
-import {React, useEffect, useInsertionEffect, useState} from 'react';
+import { React, useEffect, useInsertionEffect, useState } from 'react';
 
 export default function Main() {
+    const [joke, setJoke] = useState("");
     useEffect(() => {
-        try {
-            const response = axios.get(url);
-            console.log(response.data);
-        } catch (error) {
-            console.error("Error fetching data:", error)
-        }
+        axios.get("https://api.chucknorris.io/jokes/random")
+            .then(function (response) {
+                console.log(response.data.value);
+                setJoke(response.data.value);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     }, []);
     return (
         <main>
-
+            <h2>{joke}</h2>
         </main>
-  );
+    );
 }
 
