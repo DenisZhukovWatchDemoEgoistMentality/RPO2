ceba1ac21652ea608ac1e8416d76ab750330733e
KarasiKoff <142608861+KarasiKoff@users.noreply.github.com>
2026-02-10 09:10:44 +0300

    SEARCH

---
+++ react-app/src/components/search/Search.jsx
@@ -0,0 +1,10 @@
+import './Search.css';
+
+export default function Search() {
+  return (
+    <body>
+        <input type="text" id="InputSearch"/>
+        <Button id="Categories">КАТЕГОРИИ</Button>
+    </body>
+  );
+}
