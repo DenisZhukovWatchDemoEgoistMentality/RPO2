d4f0ba440f0a1a40cfc07c5376f39ff8e8c56924
KarasiKoff <142608861+KarasiKoff@users.noreply.github.com>
2026-02-10 11:26:42 +0300

    privacy & rules

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,8 @@ import Profile from './components/profile/Profile'
 import Messages from './components/messages/Messages'
 import Adminka from './components/adminka/Adminka'
 import Product from "./components/product/Product";
+import Rules from "./components/rules/Rules";
+import Privacy from "./components/privacy/Privacy";
 
 import './fonts/Intro.ttf';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -21,6 +23,8 @@ export default function App() {
                       {/* <Route path="/message" element={<Message />} />  */}
                       <Route path="/profile" element={<Profile />} />
                       {/* <Route path="/adminka-huinka" element={<Adminka />} /> */}
+                      <Route path="/rules" element={<Rules />} />
+                      <Route path="/privacy" element={<Privacy />} />
                   </Routes>
               </div>
       </BrowserRouter>
