1247ec92f280d9a3e445d487ea35b969f96c2023
KarasiKoff <142608861+KarasiKoff@users.noreply.github.com>
2026-02-10 07:31:43 +0300

    react base

--- react-app/src/components/main/Main.jsx
+++ react-app/src/components/main/Main.jsx
@@ -1,22 +1,40 @@
 import './Main.css';
 import axios from "axios"
 import { React, useEffect, useInsertionEffect, useState } from 'react';
+import { Navigate, Links, Link } from 'react-router-dom'
 
 export default function Main() {
-    const [joke, setJoke] = useState("");
-    useEffect(() => {
-        axios.get("https://api.chucknorris.io/jokes/random")
-            .then(function (response) {
-                console.log(response.data.value);
-                setJoke(response.data.value);
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
-    }, []);
+    // const [joke, setJoke] = useState("");
+    // useEffect(() => {
+    //     axios.get("https://api.chucknorris.io/jokes/random")
+    //         .then(function (response) {
+    //             console.log(response.data.value);
+    //             setJoke(response.data.value);
+    //         })
+    //         .catch(function (error) {
+    //             console.error(error);
+    //         });
+    // }, []);}
     return (
         <main>
-            <h2>{joke}</h2>
+            <div id="TrueMain">
+                <button className="ButtonMenu">
+                    <img src="MainImage/Vector.png" alt="" style={{ padding: "5px" }} />
+                    Главная
+                </button>
+                <button className="ButtonMenu">
+                    <img src="MainImage/Vector (1).png" alt="" style={{ padding: "5px" }} />
+                    Сообщения
+                </button>
+                <Link to="/profile" className="ButtonMenu">
+                    <img src="/MainImage/Vector (2).png" alt="" style={{ padding: "5px" }} />
+                    Профиль
+                </Link>
+                <button className="ButtonMenu">
+                    <img src="MainImage/Vector (3).png" alt="" style={{ padding: "5px" }} />
+                    Админ панель
+                </button>
+            </div>
         </main>
     );
 }
