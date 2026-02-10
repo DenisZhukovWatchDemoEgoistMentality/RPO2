c24412dd223b76383c0c78c6ff59e538404dac67
KarasiKoff <142608861+KarasiKoff@users.noreply.github.com>
2026-02-10 08:32:30 +0300

    messages & adminka

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Profile from './components/profile/Profile'
+import Messages from './components/messages/Messages'
+import Adminka from './components/adminka/Adminka'
+
 import './fonts/Intro.ttf';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -11,7 +14,10 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
+        {/* <Route path="/message" element={<Message />} />  */}
         <Route path="/profile" element={<Profile />} />
+        {/* <Route path="/adminka-huinka" element={<Adminka />} /> */}
+
       </Routes>
     </BrowserRouter>
   );
